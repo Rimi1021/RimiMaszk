4b68a7b90b8f98680148189b425f888e2e605d86
Péter Rimár <rimarpeti25@gmail.com>
2021-03-11 12:41:02 +0100

    Bpeladas Barcode kesz, listboxba még nem raktam bele

--- proba5.5/BPEladas.cs
+++ proba5.5/BPEladas.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
 
 namespace proba5._5
 {
@@ -15,6 +16,69 @@ namespace proba5._5
         public BPEladas()
         {
             InitializeComponent();
+        }
+        bool vanilyenbarcode = false;
+
+
+        /// Encode Decode gombok
+        #region Encode Decode buttonok
+        private void button_Encode_Click(object sender, EventArgs e)
+        {
+            BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+            if (textBox_Encode.Text != "")
+            {
+                pictureBox_Barcode.Image = writer.Write(textBox_Encode.Text);
+                textBox_Kosarba.Text = textBox_Encode.Text;
+            }
+            else
+            {
+                MessageBox.Show("Nincs Kódolandó vonalkód!");
+                pictureBox_Barcode.Image = null;
+                textBox_Kosarba.Text = "";
+            }
+        }
+
+        private void button_Decode_Click(object sender, EventArgs e)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            if (pictureBox_Barcode.Image != null)
+            {
+                var result = reader.Decode((Bitmap)pictureBox_Barcode.Image);
+                if (result != null)
+                {
+                    textBox_Decode.Text = result.Text;
+                    textBox_Kosarba.Text = textBox_Decode.Text;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nincs Dekódolandó vódolandó vonalkód!");
+                textBox_Kosarba.Text = "";
+                textBox_Decode.Text = "";
+            }
+        }
+        #endregion
+
+        ///Kosárba tevés melletti textboxBarcode vizsgálat
+        private void textBox_Kosarba_TextChanged(object sender, EventArgs e)
+        {
+            Raktar.Tisztalista();
+            for (int i = 0; i < SzerverData.MaszInfokOsszes.Count; i++)
+            {
+                if (SzerverData.MaszInfokOsszes[i].Barcode == textBox_Kosarba.Text)
+                {
+                    vanilyenbarcode = true;
+                    label_Adottarubrutto.Text = Convert.ToString(SzerverData.MaszInfokOsszes[i].Ar_db);
+                    label_Adottarubruttoakcio.Text = Convert.ToString(SzerverData.MaszInfokOsszes[i].Ar_db);
+                }
+            }
+            if (vanilyenbarcode == false && textBox_Kosarba.Text != "")
+            {
+                MessageBox.Show("Nem létezik ilyen vonalkód, használja a manuális termékkiválasztás opciót");
+                textBox_Decode.Text = "";
+                textBox_Encode.Text = "";
+                textBox_Kosarba.Text = "";
+            }
         }
     }
 }
